27a522bdbc26c277f0480630555513822f4b586c
Mayank <mayank.captain00@gmail.com>
2020-06-02 01:18:49 +0530

    Firebase setup & working

--- app/src/main/java/com/example/nusocial/Share_post.java
+++ app/src/main/java/com/example/nusocial/Share_post.java
@@ -1,8 +1,8 @@
 package com.example.nusocial;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Share_post extends AppCompatActivity {
 
